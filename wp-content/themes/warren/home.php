b5be4f387dfd87f756e4bf00669afff30cf42fc5
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2022-12-09 23:43:09 +0500

    fucking updraft

--- wp-content/themes/warren/home.php
+++ wp-content/themes/warren/home.php
@@ -21,7 +21,7 @@
 </section>
 
 <section class="about-warren bg-black py-60">
-    <div class="py-6 px-4 max-w-[1500px]">
+    <div class="px-6 max-w-[1500px]">
         <h1 class="text-[180px] leading-[180px] uppercase font-anekLatin font-bold text-orangeCrayola">What is Warr3n?</h1>
         <p class="text-[180px] leading-[180px] uppercase font-anekLatin font-bold text-isabelline">
             Warr3n is an imagined identity around the exploration of Web3 <span class="text-orangeCrayola">tech, art, and culture</span>. A community of <span class="text-orangeCrayola">innovators</span>.
@@ -29,7 +29,27 @@
     </div>
 </section>
 
+<section class="px-6 bg-isabelline">
 
+    <div class="sec-height" style="height: 1248px">
+        <div>
+            <div class="grid grid-cols-8 xl:grid-cols-12 content-start md:py-40">
+                <div class="col-span-6 mb-4 max-w-md xl:col-span-8 xl:col-start-2 2xl:col-span-7 2xl:col-start-2">
+                    <h2 class="font-medium font-anekLatin uppercase 2xl:text-9xl mb-4">Explore</h2>
+                </div>
+
+                <div class="col-span-1 col-end-9 xl:col-end-11 2xl:row-start-2">
+                    <img class="w-3/5" src="http://localhost/warren/wp-content/uploads/2022/12/warren-v-logo-e1670610973470.png" alt="warren vertical Logo">
+                </div>
+
+                <div class="col-span-6 mb-4 max-w-md xl:col-span-8 xl:col-start-2 2xl:col-span-7 2xl:col-start-2">
+                    <p class="font-normal font-anekLatin text-2xl">Warr3n is an imagined identity around the exploration of Web3 tech, art, and culture. A community of innovators.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
 
 
 
