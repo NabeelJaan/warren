13c2fb79b085e4547e623dcb54e847042a602b7a
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2022-12-21 23:51:22 +0500

    GB Blocks
    All req GB created and also created custom fields

--- wp-content/themes/warren/inc/gb-blocks.php
+++ wp-content/themes/warren/inc/gb-blocks.php
@@ -18,11 +18,88 @@ function exponentialGrowth_acf_block() {
 
             "render_template" => "inc/blocks/hero-sec.php",
 
-            "category" => "cemeterySearch",
+            "category" => "warren",
 
             "icon" => "block-default",
 
             "keywords" => ["Hero Section", ""],
+        ]);
+
+
+        // About Section
+
+        acf_register_block_type([
+
+            "name" => __("About Section"),
+
+            "title" => __("About Section"),
+
+            "description" => __("About Section on home page"),
+
+            "render_template" => "inc/blocks/about.php",
+
+            "category" => "warren",
+
+            "icon" => "block-default",
+
+            "keywords" => ["About Section", ""],
+        ]);
+
+        // Article one
+
+        acf_register_block_type([
+
+            "name" => __("Article One"),
+
+            "title" => __("Article One"),
+
+            "description" => __("Article One on home page"),
+
+            "render_template" => "inc/blocks/featured-articles.php",
+
+            "category" => "warren",
+
+            "icon" => "block-default",
+
+            "keywords" => ["Article One", ""],
+        ]);
+
+        // Latest Articles
+
+        acf_register_block_type([
+
+            "name" => __("Latest Articles"),
+
+            "title" => __("Latest Articles"),
+
+            "description" => __("Latest Articles on home page"),
+
+            "render_template" => "inc/blocks/latest-articles.php",
+
+            "category" => "warren",
+
+            "icon" => "block-default",
+
+            "keywords" => ["Latest Articles", ""],
+        ]);
+
+        // FAQ's
+
+        acf_register_block_type([
+
+            "name" => __("FAQ"),
+
+            "title" => __("FAQ"),
+
+            "description" => __("FAQ on home page"),
+
+            "render_template" => "inc/blocks/faq.php",
+
+            "category" => "warren",
+
+            "icon" => "block-default",
+
+            "keywords" => ["FAQ", ""],
         ]);
     }
 }
